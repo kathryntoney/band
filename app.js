6707ef00129544e0c42f01424450dbf992a3b2ba
Katie Toney <kathryn.toney@gmail.com>
2023-05-02 15:56:55 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+let app = express();
+let PORT = 3000;
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.use(require('./routes/index'))
+app.use(require('./routes/albums'))
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+})
