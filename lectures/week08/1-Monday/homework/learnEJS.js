335efe531fbe6cf088683bbbda33e9684b9a4b5f
Katie Toney <kathryn.toney@gmail.com>
2023-05-02 10:07:13 -0400

    tues am commit

--- lectures/week08/1-Monday/homework/learnEJS.js
+++ lectures/week08/1-Monday/homework/learnEJS.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
     res.render('home') // don't have to include .ejs bc we used default folder
 })
 
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', { random: num })
+})
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit })
+})
+
 app.listen(PORT, () => { // port, callback fxn
     console.log(`listening on port: ${PORT}`);
 })
