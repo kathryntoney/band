3f341a91427fa6b8812236ffddf2f8de01e1f966
Katie Toney <kathryn.toney@gmail.com>
2023-05-03 10:36:30 -0400

    weds am

--- routes/albums.js
+++ routes/albums.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+let dataFile = require('../data/data.json')
+let albumData = dataFile.albums;
 
 router.get('/albums', (req, res) => {
-    res.send('albums')
+    let trackList = [];
+    albumData.forEach(albumObj => {
+        trackList = trackList.concat(albumObj.trackList)
+    })
+
+    res.render('albums', {
+        trackList: trackList,
+        albums: albumData,
+    })
 })
+
+// router.get('/albums/:album', (req, res) => {
+//     let album = req.params.album;
+//     res.render('album', { album })
+// })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('band')
+router.get('/', (req, res) => {
+    // res.send('band')
+    res.render('index')
 })
 
 module.exports = router;
